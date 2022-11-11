84cadd93d7733f017ee8c2178d0e96521a8bcc8f
Omar <omar.elwakil@vodafone.com>
2022-11-11 21:30:47 +0200

    adjsut proxy between services

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -7,8 +7,8 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 
-app.use('/customers', proxy('http://localhost:5001'))
-app.use('/products', proxy('http://localhost:5002'))
+app.use('/customers', proxy('customers-container:5001'))
+app.use('/products', proxy('products-container:5002'))
 
 app.get('/', (req, res) => {
     res.send('App is responding')
